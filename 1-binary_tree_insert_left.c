41f1750f141f8f21d3eeff07ebe7cfd6864e8209
sanotogii <nasaereh@gmail.com>
2023-10-31 19:29:45 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,9 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
+
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
