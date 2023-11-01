ea74ba3cfade515aa019cf0094e0b06b3e749439
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-01 11:07:41 +0100

    3-binary_tree_delete.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -25,5 +25,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = new_node;
 	}
 	parent->right = new_node;
-	return (new_node);
+	return (new_node); 
 }
