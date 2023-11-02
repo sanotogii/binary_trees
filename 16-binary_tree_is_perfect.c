802b82b3ceed83bec80a096e56bfa5b1b58471a1
sanotogii <nasaereh@gmail.com>
2023-11-02 19:01:11 +0100

    task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+    left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+    if (left_height == right_height)
+        return (1);
+    return (0);
+}
