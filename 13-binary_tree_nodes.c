3524e9be7aa5ef24b6ad615aecb2b741536efa9d
sanotogii <nasaereh@gmail.com>
2023-11-02 09:03:00 +0100

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,14 +14,11 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t right_node, left_node;
-
 	if (tree == NULL)
 		return (0);
-	right_node = binary_tree_nodes(tree->right);
-	left_node = binary_tree_nodes(tree->left);
-	if (tree->right || tree->right)
-		return (1 + right_node + left_node);
-	else
-		return (right_node + left_node);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
